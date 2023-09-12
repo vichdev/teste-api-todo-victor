7a8281e86a3a9a4fac85c23b88527bdba8625fa5
VictorHugo <victor.hugo.antunes.n@gmail.com>
2023-09-11 23:57:01 -0300

    refactor: refatora todo service
    adiciona novos metodos

--- src/Kobold.TodoApp.Api/Services/TodoService.cs
+++ src/Kobold.TodoApp.Api/Services/TodoService.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Kobold.TodoApp.Api.Models;
 
 namespace Kobold.TodoApp.Api.Services
@@ -12,39 +9,136 @@ namespace Kobold.TodoApp.Api.Services
     public class TodoService
     {
 
-        private static int nextId = 1;
         private static List<Todo> Todos = new List<Todo>();
+        private static List<TodoCollection> TodoCollections = new List<TodoCollection>();
 
-        public IEnumerable<Todo> Get()
+        public TodoCollection GetTodoCollectionById(Guid todoCollectionId)
+        {
+
+            var todoCollections = TodoCollections.FirstOrDefault(collection => collection.Id == todoCollectionId);
+
+           return todoCollections == null ? throw new ApplicationException("Esta coleção de tarefas não existe.") :  todoCollections;
+        }
+
+        public List<TodoCollection> GetTodosCollection()
+        {
+            return TodoCollections;
+        }
+
+        public void CreateTodoCollection(TodoCollectionViewModel todoCollection)
+        {
+           
+                var collection = new TodoCollection { Name = todoCollection.Name, Todos = new List<Todo>() };
+
+                TodoCollections.Add(collection);
+            
+
+        }
+
+        public void AddTodoToCollection(Guid todoCollectionId, Guid todoId)
+        {
+            var todoCollection = GetTodoCollectionById(todoCollectionId);
+            var todo = Todos.FirstOrDefault(todo => todo.Id == todoId);
+
+            if (todoCollection != null & todo != null)
+            {
+                todoCollection.Todos.Add(todo);
+            } else
+            {
+                throw new ApplicationException("Não foi possível adicionar a tarefa nessa coleção.");
+
+            }
+        }
+
+        public void RemoveTodoFromCollection(Guid todoCollectionId, Guid todoId) 
+        {
+            var todoCollection = GetTodoCollectionById(todoCollectionId);
+            var todo = todoCollection.Todos.FirstOrDefault(todo => todo.Id == todoId);
+
+
+            if (todoCollection != null & todo != null)
+            {
+                todoCollection.Todos.Remove(todo);
+            }
+            else
+            {
+                throw new ApplicationException("Não foi possível remover a tarefa desta coleção.");
+
+            }
+        }
+
+        public void UpdateTodoFromCollection(Guid todoCollectionId, Todo todoViewModel)
+        {
+            var todoCollection = GetTodoCollectionById(todoCollectionId);
+            var todo = todoCollection.Todos.FirstOrDefault(todo => todo.Id == todoViewModel.Id);
+
+            if (todoCollection != null & todo != null)
+            {
+                todo.Description = todoViewModel.Description;
+                todo.Done = todoViewModel.Done;
+            } else
+            {
+                throw new ApplicationException("Não foi possível remover a tarefa desta coleção.");
+            }
+        }
+
+        public void CreateTodo(TodoViewModel todoViewModel)
+        {
+            var todo = new Todo
+            {
+                DataCriacao = DateTime.Now,
+                Description = todoViewModel.Description,
+                Done = todoViewModel.Done,
+            };
+
+            Todos.Add(todo);
+        }
+
+        public void UpdateTodo(Guid id, TodoViewModel todovm)
+        {
+            var todo = Todos.Single(todo => todo.Id == id);
+            if (todo != null)
+            {
+                todo.Done = todovm.Done;
+            } else
+            {
+                throw new ApplicationException("Não foi possível atualizar a tarefa.");
+
+            }
+
+
+        }
+
+        public Todo GetTodoById(Guid id)
+        {
+            var todosById = Todos.Single(todo => todo.Id == id);
+            if(todosById != null)
+            {
+                return todosById;
+            } else
+            {
+                throw new ApplicationException("Não foi possível encontrar esta coleção.");
+
+            }
+        }
+
+        public List<Todo> GetTodos()
         {
             return Todos;
         }
 
-        public Todo Create(TodoViewModel todovm)
-        {
-            var todo = new Todo { Id = nextId++, DataCriacao = DateTime.Now, Description = todovm.Description, Done = todovm.Done }; 
-            Todos.Add(todo);
-
-            return todo;
-        }
-
-        public Todo Update(int id, TodoViewModel todovm)
-        {
-            var todo = Todos.Single(todo => todo.Id == id);
-            todo.Done = todovm.Done;
-
-            return todo;
-        }
-
-        public Todo Get(int id)
-        {
-            return Todos.Single(todo => todo.Id == id);
-        }
-
-        public void Remove(int id)
+        public void RemoveTodo(Guid id)
         {
             var index = Todos.FindIndex(todo => todo.Id == id);
+            if (index == -1)
+            {
+
             Todos.RemoveAt(index);
+            } else
+            {
+                throw new ApplicationException("Não foi possível remover esta tarefa.");
+
+            }
         }
 
     }

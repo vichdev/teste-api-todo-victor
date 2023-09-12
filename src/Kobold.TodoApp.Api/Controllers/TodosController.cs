1024fc4f036ec250cb007a4d8dc4a6a1a107c9eb
VictorHugo <victor.hugo.antunes.n@gmail.com>
2023-09-11 23:53:48 -0300

    feat: adiciona collection endpoints

--- src/Kobold.TodoApp.Api/Controllers/TodosController.cs
+++ src/Kobold.TodoApp.Api/Controllers/TodosController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Kobold.TodoApp.Api.Models;
 using Kobold.TodoApp.Api.Services;
+
 
 namespace Kobold.TodoApp.Api.Controllers
 {
@@ -18,40 +17,87 @@ namespace Kobold.TodoApp.Api.Controllers
         private readonly TodoService _todoService;
         private readonly ILogger<TodosController> _logger;
 
+
+
         public TodosController(ILogger<TodosController> logger)
         {
             _logger = logger;
             _todoService = new TodoService();
         }
 
+        [HttpPost("collection")]
+        public IActionResult CreateCollection([FromBody] TodoCollectionViewModel todoCollectionViewModel)
+        {
+          
+            _todoService.CreateTodoCollection(todoCollectionViewModel);
+
+            return Ok("Coleção de tarefas criada com sucesso.");
+     
+        }
+
+
+        [HttpGet("collection")]
+        public List<TodoCollection> GetCollection()
+        {
+            return _todoService.GetTodosCollection();
+        }
+
+        [HttpGet("collection/{id}")]
+        public void GetCollectionById([FromRoute] Guid id)
+        { 
+       
+            _todoService.GetTodoCollectionById(id);
+
+        }
+
+        [HttpPost("collection/addTodo")]
+        public void AddTodoToCollection([FromBody] TodoCollectionViewModel todoCollectionViewModel)
+        {
+          
+            _todoService.AddTodoToCollection(todoCollectionViewModel.Id, todoCollectionViewModel.Todo.Id);
+             
+        }
+
+        [HttpDelete("collection/removeTodo")]
+        public void RemoveTodoFromCollection([FromBody] TodoCollectionViewModel todoCollectionViewModel)
+        {
+            _todoService.RemoveTodoFromCollection(todoCollectionViewModel.Id, todoCollectionViewModel.Todo.Id);
+        }
+
+        [HttpPut("collection/updateTodo")]
+        public void UpdateTodoFromCollection([FromBody] TodoCollectionViewModel todoCollectionViewModel)
+        {
+            _todoService.UpdateTodoFromCollection(todoCollectionViewModel.Id, todoCollectionViewModel.Todo);
+        }
+
         [HttpGet]
         public IEnumerable<Todo> Get()
         {
-            return _todoService.Get();
+            return _todoService.GetTodos();
         }
 
         [HttpPost]
-        public Todo Create([FromBody] TodoViewModel todovm)
+        public void Create([FromBody] TodoViewModel todovm)
         {
-            return _todoService.Create(todovm);
+            _todoService.CreateTodo(todovm);
         }
 
         [HttpPut("{id}")]
-        public Todo Update([FromRoute] int id, [FromBody] TodoViewModel todovm)
+        public void Update([FromRoute] Guid id, [FromBody] TodoViewModel todovm)
         {
-            return _todoService.Update(id, todovm);
+            _todoService.UpdateTodo(id, todovm);
         }
 
         [HttpGet("{id}")]
-        public Todo Get([FromRoute] int id)
+        public Todo Get([FromRoute] Guid id)
         {
-            return _todoService.Get(id);
+            return _todoService.GetTodoById(id);
         }
 
         [HttpDelete("{id}")]
-        public void Remove([FromRoute] int id)
+        public void Remove([FromRoute] Guid id)
         {
-            _todoService.Remove(id);
+            _todoService.RemoveTodo(id);
         }
 
     }

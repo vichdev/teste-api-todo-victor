6b79e18e1b7b5ba83368b25e22beeda459a66f6b
VictorHugo <victor.hugo.antunes.n@gmail.com>
2023-09-11 23:56:24 -0300

    feat: adiciona models collection

---
+++ src/Kobold.TodoApp.Api/Models/TodoCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kobold.TodoApp.Api.Models
+{
+    public class TodoCollection
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Todo> Todos { get; set; } = new List<Todo>();
+
+        public TodoCollection()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/Kobold.TodoApp.Api/Models/TodoCollectionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kobold.TodoApp.Api.Models
+{
+    public class TodoCollectionViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Todo Todo { get; set; }
+    }
+}
